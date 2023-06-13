cebbaad65db8db5fc3f30ffe9f46cc05006418c1
PaulaPerazzo <mpperazzosampaio@gmail.com>
2023-06-13 15:11:12 -0300

    (develop) creating chunks

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def main():
     load_dotenv()
@@ -16,7 +17,17 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
         
-        st.write(text)
+        # split into chunks
+        text_spliter = CharacterTextSplitter(
+            separator = "\n",
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            length_function=len
+        )
+
+        chunks = text_spliter.split_text(text)
+
+        st.write(chunks)
 
 
 if __name__ == "__main__":
