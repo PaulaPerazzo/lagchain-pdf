1729d61716bb6640ba6038d64281cbceb6f353f6
PaulaPerazzo <mpperazzosampaio@gmail.com>
2023-06-13 12:32:19 -0300

    (develop) pdf extracted

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from dotenv import load_dotenv 
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
 def main():
     load_dotenv()
-    print('hello world')
+    st.set_page_config(page_title="ask your pdf")
+    st.header("ask your PDF")
+
+    pdf = st.file_uploader("upload your PDF", type="pdf")
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        st.write(text)
 
 
 if __name__ == "__main__":
     main()
- 
